ef39c5d76e160b99b8183a02011fff4113f4da0e
Yashwanth Anand S <yashwanthanands@outlook.com>
2023-06-21 07:34:12 +0530

    Added the appDAO inject logics to save the instructor

--- 01-jpa-one-to-one-unidirectional/src/main/java/com/yash/dev/CruddemoApplication.java
+++ 01-jpa-one-to-one-unidirectional/src/main/java/com/yash/dev/CruddemoApplication.java
@@ -1,5 +1,8 @@
 package com.yash.dev;
 
+import com.yash.dev.dao.AppDAO;
+import com.yash.dev.entity.Instructor;
+import com.yash.dev.entity.InstructorDetail;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,10 +16,23 @@ public class CruddemoApplication {
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner(String[] args) {
+	public CommandLineRunner commandLineRunner(AppDAO appDAO) {
 		return runner -> {
-			System.out.println("Hello World");
+			createInstructor(appDAO);
 		};
+	}
+
+	private void createInstructor(AppDAO appDAO) {
+		Instructor tempInstructor = new Instructor("Chad","Darby","chad@gmail.com");
+
+		InstructorDetail tempInstructorDetail=new InstructorDetail("http://www.yash.com/chad",
+						"Luv 2 Code!!!");
+
+		tempInstructor.setInstructorDetail(tempInstructorDetail);
+
+		System.out.println("Saving the instructor "+tempInstructor);
+		appDAO.save(tempInstructor);
+		System.out.println("Saved successfully");
 	}
 
 }
