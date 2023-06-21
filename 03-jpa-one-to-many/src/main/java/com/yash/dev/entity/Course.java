2cabad31a0fbc9c7a3fc4d6c8e5d66ef5248afea
Yashwanth Anand S <yashwanthanands@outlook.com>
2023-06-21 18:54:28 +0530

    Created Course Entity class

---
+++ 03-jpa-one-to-many/src/main/java/com/yash/dev/entity/Course.java
@@ -0,0 +1,64 @@
+package com.yash.dev.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+/**
+ * @author yashwanthanands
+ */
+
+@Entity
+@Table(name="course")
+public class Course {
+
+    private int id;
+
+    private String title;
+
+    private Instructor instructor;
+
+    public Course() {
+    }
+
+    public Course(String title) {
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Instructor getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(Instructor instructor) {
+        this.instructor = instructor;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                '}';
+    }
+}
