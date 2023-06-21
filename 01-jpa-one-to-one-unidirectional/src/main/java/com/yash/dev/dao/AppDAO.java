fee3f5181508573f1854722ba9bba5acea38b51a
Yashwanth Anand S <yashwanthanands@outlook.com>
2023-06-21 07:28:29 +0530

    Implemented the DAO logics for save instructor

---
+++ 01-jpa-one-to-one-unidirectional/src/main/java/com/yash/dev/dao/AppDAO.java
@@ -0,0 +1,11 @@
+package com.yash.dev.dao;
+
+import com.yash.dev.entity.Instructor;
+
+/**
+ * @author yashwanthanands
+ */
+public interface AppDAO {
+
+    void save(Instructor theInstructor);
+}
---
+++ 01-jpa-one-to-one-unidirectional/src/main/java/com/yash/dev/dao/AppDAOImpl.java
@@ -0,0 +1,25 @@
+package com.yash.dev.dao;
+
+import com.yash.dev.entity.Instructor;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityManager;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * @author yashwanthanands
+ */
+public class AppDAOImpl implements AppDAO{
+
+
+    private EntityManager entityManager;
+
+    public AppDAOImpl(EntityManager enMgr) {
+        this.entityManager=enMgr;
+    }
+
+    @Override
+    @Transactional
+    public void save(Instructor theInstructor) {
+        entityManager.persist(theInstructor);
+    }
+}
