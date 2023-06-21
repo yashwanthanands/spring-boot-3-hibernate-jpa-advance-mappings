2c73d0510d0c5c1059996c85850022bbf2223d66
Yashwanth Anand S <yashwanthanands@outlook.com>
2023-06-21 10:36:35 +0530

    Added the runner method to trigger findInstructorById

--- 01-jpa-one-to-one-unidirectional/src/main/java/com/yash/dev/CruddemoApplication.java
+++ 01-jpa-one-to-one-unidirectional/src/main/java/com/yash/dev/CruddemoApplication.java
@@ -18,8 +18,18 @@ public class CruddemoApplication {
 	@Bean
 	public CommandLineRunner commandLineRunner(AppDAO appDAO) {
 		return runner -> {
-			createInstructor(appDAO);
+			//createInstructor(appDAO);
+			  findInstructor(appDAO);
 		};
+	}
+
+	private void findInstructor(AppDAO appDAO) {
+		int theId=1;
+		System.out.println("Finding instructor id : "+ theId);
+		Instructor tempInstructor = appDAO.findInstructorById(theId);
+		System.out.println(" Instructor Profile "+ tempInstructor);
+		System.out.println(" Instructor Details "+ tempInstructor.getInstructorDetail());
+
 	}
 
 	private void createInstructor(AppDAO appDAO) {
